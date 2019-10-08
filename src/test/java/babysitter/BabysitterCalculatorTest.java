04cb9ac077abf9db847132d03c33e0662da4daa6
Kiara Atkins <atkinsk98@gmail.com>
2019-10-08 09:33:37 -0400

    Refactored by adding setUp method to minimize code duplication.

--- src/test/java/babysitter/BabysitterCalculatorTest.java
+++ src/test/java/babysitter/BabysitterCalculatorTest.java
@@ -1,25 +1,29 @@
 package babysitter;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BabysitterCalculatorTest {
+    BabysitterCalculator babysitterCalculator;
+
+    @Before
+    public void setUp() {
+        babysitterCalculator = new BabysitterCalculator();
+    }
 
     @Test
     public void testReturnsFifteenDollarsForOneHourWithFamilyA() {
-        BabysitterCalculator babysitterCalculator = new BabysitterCalculator();
         Assertions.assertThat(babysitterCalculator.findTotal("9:00 PM", "10:00 PM", "A")).isEqualTo("$15.00");
     }
 
     @Test
     public void testReturnsThirtyDollarsForTwoHoursWithFamilyA() {
-        BabysitterCalculator babysitterCalculator = new BabysitterCalculator();
         Assertions.assertThat(babysitterCalculator.findTotal("8:00 PM", "10:00 PM", "A")).isEqualTo("$30.00");
     }
 
     @Test
     public void testReturnsThirtyFiveDollarsForOneHourBeforeElevenPmAndOneHourAfterWithFamilyA() {
-        BabysitterCalculator babysitterCalculator = new BabysitterCalculator();
         Assertions.assertThat(babysitterCalculator.findTotal("10:00 PM", "12:00 PM", "A")).isEqualTo("$35.00");
     }
 }
