e27cce0c7f250a244440e59372e8d6380a4cd741
Kiara Atkins <atkinsk98@gmail.com>
2019-10-08 11:02:05 -0400

    Refactored by breaking up a findTotal into saller methods for readability.

--- src/main/java/babysitter/BabysitterCalculator.java
+++ src/main/java/babysitter/BabysitterCalculator.java
@@ -3,32 +3,47 @@ package babysitter;
 public class BabysitterCalculator {
 
     public String findTotal(String startTime, String endTime, String family) {
-        int startHour = Integer.parseInt(startTime.split(":")[0]);
-        int endHour = Integer.parseInt(endTime.split(":")[0]);
+        int startHour = parseTime(startTime);
+        int endHour = parseTime(endTime);
 
-        int startAtMilitaryTime;
-        if (startHour < 5) {
-            startAtMilitaryTime = startHour;
+        int startAtMilitaryTime = convertTimeToMilitary(startHour);
+        int endAtMilitaryTime = convertTimeToMilitary(endHour);
+
+        int lateNightHours = findLateNightHours(startAtMilitaryTime, endAtMilitaryTime);
+
+        return "$" + calculateTotal(endHour, startHour, lateNightHours) + ".00";
+
+    }
+
+    private int parseTime(String time) {
+        int hour = Integer.parseInt(time.split(":")[0]);
+        return hour;
+    }
+
+    private int convertTimeToMilitary(int hour) {
+        int militaryTime;
+        if (hour < 5) {
+            militaryTime = hour;
         }
         else {
-            startAtMilitaryTime = startHour + 12;
+            militaryTime = hour + 12;
         }
+        return militaryTime;
+    }
 
-        int endAtMilitaryTime;
-        if (endHour < 5) {
-            endAtMilitaryTime = endHour;
-        }
-        else {
-            endAtMilitaryTime = endHour + 12;
-        }
-
+    private int findLateNightHours (int startTimeInMilitary, int endTimeInMilitary) {
         int lateNightHours = 0;
-        for (int i = startAtMilitaryTime; i < endAtMilitaryTime; i++) {
+
+        for (int i = startTimeInMilitary; i < endTimeInMilitary; i++) {
             if (i < 5 || i > 22) {
                 lateNightHours++;
             }
         }
 
-        return "$" + (((endHour - startHour) * 15) + (lateNightHours * 5)) + ".00";
+        return lateNightHours;
+    }
+
+    private int calculateTotal( int endHour, int startHour, int lateNightHours) {
+        return ((endHour - startHour) * 15) + (lateNightHours * 5);
     }
 }
