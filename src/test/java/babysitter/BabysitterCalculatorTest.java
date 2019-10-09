5dfe763441d47fe2c70c217e63b3bc0d226268f3
Kiara Atkins <atkinsk98@gmail.com>
2019-10-08 20:29:46 -0400

    Refactored tests with static import statement of Assertions.

--- src/test/java/babysitter/BabysitterCalculatorTest.java
+++ src/test/java/babysitter/BabysitterCalculatorTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class BabysitterCalculatorTest {
     BabysitterCalculator babysitterCalculator;
 
@@ -14,86 +16,91 @@ public class BabysitterCalculatorTest {
 
     @Test
     public void testReturnsFifteenDollarsForOneHourWithFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("9:00 PM", "10:00 PM", "A")).isEqualTo("$15.00");
+        assertThat(babysitterCalculator.findTotal("9:00 PM", "10:00 PM", "A")).isEqualTo("$15.00");
     }
 
     @Test
     public void testReturnsThirtyDollarsForTwoHoursWithFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("8:00 PM", "10:00 PM", "A")).isEqualTo("$30.00");
+        assertThat(babysitterCalculator.findTotal("8:00 PM", "10:00 PM", "A")).isEqualTo("$30.00");
     }
 
     @Test
     public void testReturnsThirtyFiveDollarsForOneHourBeforeElevenPmAndOneHourAfterWithFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("10:00 PM", "12:00 PM", "A")).isEqualTo("$35.00");
+        assertThat(babysitterCalculator.findTotal("10:00 PM", "12:00 PM", "A")).isEqualTo("$35.00");
     }
 
     @Test
     public void testReturnsFortyDollarsForTwoLateNightHoursFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("2:00 AM", "4:00 AM", "A")).isEqualTo("$40.00");
+        assertThat(babysitterCalculator.findTotal("2:00 AM", "4:00 AM", "A")).isEqualTo("$40.00");
     }
 
     @Test
     public void testReturnsOneHundredNinetyDollarsForFullNightWithFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "4:00 AM", "A")).isEqualTo("$190.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "4:00 AM", "A")).isEqualTo("$190.00");
     }
 
     @Test
     public void testReturnsFiftyDollarsWorkingNineToMidnightFamilyA() {
-        Assertions.assertThat(babysitterCalculator.findTotal("9:00 PM", "12:00 PM", "A")).isEqualTo("$50.00");
+        assertThat(babysitterCalculator.findTotal("9:00 PM", "12:00 PM", "A")).isEqualTo("$50.00");
     }
 
     @Test
     public void testReturnsTwelveDollarsForOneEarlyEveningHourFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "6:00 PM", "B")).isEqualTo("$12.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "6:00 PM", "B")).isEqualTo("$12.00");
     }
 
     @Test
     public void testReturnsTwentyFourDollarsForTwoEarlyEveningHoursFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "7:00 PM", "B")).isEqualTo("$24.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "7:00 PM", "B")).isEqualTo("$24.00");
     }
 
     @Test
     public void testReturnsEightDollarsForTenToElevenFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("10:00 PM", "11:00 PM", "B")).isEqualTo("$8.00");
+        assertThat(babysitterCalculator.findTotal("10:00 PM", "11:00 PM", "B")).isEqualTo("$8.00");
     }
 
     @Test
     public void testReturnsTwentyDollarsForNineToElevenFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("9:00 PM", "11:00 PM", "B")).isEqualTo("$20.00");
+        assertThat(babysitterCalculator.findTotal("9:00 PM", "11:00 PM", "B")).isEqualTo("$20.00");
     }
 
     @Test
     public void testReturnsSixteenDollarsForOneHourLateNightFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("1:00 PM", "2:00 AM", "B")).isEqualTo("$16.00");
+        assertThat(babysitterCalculator.findTotal("1:00 PM", "2:00 AM", "B")).isEqualTo("$16.00");
     }
 
     @Test
     public void testReturnsTwentyFourDollarsForBetweenElevenAndOneFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("11:00 PM", "1:00 AM", "B")).isEqualTo("$24.00");
+        assertThat(babysitterCalculator.findTotal("11:00 PM", "1:00 AM", "B")).isEqualTo("$24.00");
     }
 
     @Test
     public void testReturnsOneHundredFortyDollarsForFullNightFamilyB() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "4:00 AM", "B")).isEqualTo("$140.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "4:00 AM", "B")).isEqualTo("$140.00");
     }
 
     @Test
     public void testReturnsTwentyOneDollarsForOneHourFamilyC() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "6:00 PM", "C")).isEqualTo("$21.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "6:00 PM", "C")).isEqualTo("$21.00");
     }
 
     @Test
     public void testReturnsFortyTwoDollarsForTwoHoursFamilyC() {
-        Assertions.assertThat(babysitterCalculator.findTotal("5:00 PM", "7:00 PM", "C")).isEqualTo("$42.00");
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "7:00 PM", "C")).isEqualTo("$42.00");
     }
 
     @Test
     public void testReturnsFifteenDollarsForOneLateNightHourFamilyC() {
-        Assertions.assertThat(babysitterCalculator.findTotal("10:00 PM", "11:00 PM", "C")).isEqualTo("$15.00");
+        assertThat(babysitterCalculator.findTotal("10:00 PM", "11:00 PM", "C")).isEqualTo("$15.00");
     }
 
     @Test
     public void testReturnsThirtySixDollarsForOneStandardHourAndOneLateHourFamilyC() {
-        Assertions.assertThat(babysitterCalculator.findTotal("8:00 PM", "10:00 PM", "C")).isEqualTo("$36.00");
+        assertThat(babysitterCalculator.findTotal("8:00 PM", "10:00 PM", "C")).isEqualTo("$36.00");
+    }
+
+    @Test
+    public void testReturnsTwoHundredSevenForOneNightFamilyC() {
+        assertThat(babysitterCalculator.findTotal("5:00 PM", "4:00 AM", "C")).isEqualTo("$189.00");
     }
 }
